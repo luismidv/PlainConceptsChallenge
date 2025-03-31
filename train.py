0ca34cf92c105fc2a366f14abb17e85e54f671a4
Luis Miguel Pérez <perezmartluismiguel@gmail.com>
2025-03-31 17:05:19 +0200

    Commite

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def model_training():
     ultralytics.settings.update({'datasets_dir': "./dataset"})
     #save_path = '/tmp/runs/detect/train2'
-    Path(save_path).mkdir(parents=True, exist_ok=True)
+    #Path(save_path).mkdir(parents=True, exist_ok=True)
     model = YOLO("yolov8n.pt")
     model.train(data="datasets/data/dataset.yaml", epochs=20, imgsz=384, batch = 2, device="cpu",verbose=True)
 
