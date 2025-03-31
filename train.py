1570bf47b8e5c2e6eafb421948511aaf9bbbef7f
Luis Miguel Pérez Martín <luismidv@users.noreply.huggingface.co>
2025-03-31 14:43:31 +0000

    Upload train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ def model_training():
     save_path = "/tmp/runs/detect/train2"
     Path(save_path).mkdir(parents=True, exist_ok=True)
     model = YOLO("yolov8n.pt")
-    model.train(data="datasets/data/dataset.yaml", epochs=20, imgsz=384, batch = 2, device="cpu", save_dir =save_path)
+    model.train(data="datasets/data/dataset.yaml", epochs=20, imgsz=384, batch = 2, device="cpu", save_dir =save_path,verbose=True)
 
 
 
